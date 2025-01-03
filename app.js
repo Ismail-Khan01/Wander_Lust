83498ac923599a45ed6170a9d3fa8cfc295f51de
Ismail khan <ismailkhan.dev01@gmail.com>
2025-01-03 22:13:47 +0530

    created Model Schema using mongoose

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const port = 8000
+const list = require("./models/listing")
 
 const mongo_url = "mongodb://127.0.0.1:27017/wanderLust"
 async function main() {
@@ -13,6 +14,16 @@ main().then(() => {
     console.log(err)
 })
 
+app.get("/list", (req, res) => {
+    const list1 = new list({
+        title: "ismail's villa",
+        description: "A place best for vocation",
+        price: 10000,
+        location: "Banglore",
+        country: "India"
+    })
+    res.send("received the request");
+})
 
 app.get("/", (req, res) => {
     console.log("the get request on / route")

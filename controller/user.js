befab6ef601815d596432336719dc2997020db43
Ismail khan <ismailkhan.dev01@gmail.com>
2025-01-13 18:40:31 +0530

    refactoring listingRouter using router.route

--- controller/user.js
+++ controller/user.js
@@ -1,4 +1,4 @@
-
+const user = require("../models/user.js")
 module.exports.renderSignupForm = (req, res) => {
     res.render("./users/signup.ejs")
 }
--- router/listings.js
+++ router/listings.js
@@ -5,27 +5,28 @@ const asyncWrap = require("../utils/asyncWrap.js");
 const { isLoggedIn, isOwner, listingValidator } = require("../middleware.js");
 const listingController = require("../controller/listings.js");
 
+// route to view all listings and
+//route for adding new listing 
+router.route("/")
+    .get("/", asyncWrap(listingController.index))
+    .post("/", listingValidator, asyncWrap(listingController.addNewListing))
 
-// route to view all listings
-router.get("/", asyncWrap(listingController.index))
 
 // route to get new Listing form
 router.get("/new", isLoggedIn, listingController.newListingForm)
 
-//route for adding new listing 
-router.post("/", listingValidator, asyncWrap(listingController.addNewListing))
 
-// route to view individual listing
-router.get("/:id", asyncWrap(listingController.showListing))
+
+// route to view individual listing,
+// route to update the listing and
+// delte route for deleting the listing
+router.route("/:id")
+    .get("/:id", asyncWrap(listingController.showListing))
+    .put("/:id", isLoggedIn, isOwner, listingValidator, asyncWrap(listingController.updateListing))
+    .delete("/:id", isLoggedIn, isOwner, asyncWrap(listingController.destroyListing))
 
 
 // route to edit the listing
 router.get("/:id/edit", isLoggedIn, isOwner, asyncWrap(listingController.showEditForm))
-
-// route to update the listing
-router.put("/:id", isLoggedIn, isOwner, listingValidator, asyncWrap(listingController.updateListing))
-
-// delte route for deleting the listing
-router.delete("/:id", isLoggedIn, isOwner, asyncWrap(listingController.destroyListing))
 
 module.exports = router
